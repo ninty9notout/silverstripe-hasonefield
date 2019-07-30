91872d89a65d68dd3a75ce46ac3e472bd660ec6b
hirenpatel <hiren.patel@mademedia.co.uk>
2019-07-30 15:05:56 +0100

    Adding function to get grid field

--- client/src/legacy/hasonefield.js
+++ client/src/legacy/hasonefield.js
@@ -5,6 +5,9 @@ jQuery.entwine('ss', ($) => {
     $.entwine('ss', function($) {
         // Covers both tabular delete button, and the button on the detail form
         $('.grid-field.hasonebutton .add-existing-autocompleter .action_gridfield_relationadd').entwine({
+            getGridField: function() {
+                return this.closest('.grid-field');
+            },
             onclick: function(e) {
                 this._super(e);
 
